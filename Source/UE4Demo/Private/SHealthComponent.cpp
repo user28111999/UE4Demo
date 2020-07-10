3f0c0aff821b9023c8cbe6e9ae32a79c003e3dc4
Alan <contact@yunseok.dev>
2020-07-11 00:28:09 +0200

    Create Health Component

---
+++ Source/UE4Demo/Private/SHealthComponent.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SHealthComponent.h"
+
+// Sets default values
+ASHealthComponent::ASHealthComponent()
+{
+	Health = 100;
+}
+
+// Called when the game starts or when spawned
+void ASHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
